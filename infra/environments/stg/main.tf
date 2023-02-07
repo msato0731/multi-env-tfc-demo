6d48273b49dd487163eeb72ec858a8bfc1a79047
msato0731 <rhcq.3127@gmail.com>
2023-02-07 16:27:42 +0900

    update: stg

--- infra/environments/stg/main.tf
+++ infra/environments/stg/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
     tags = {
       Env       = "stg"
       Terraform = "true"
-      SystemName = "tfc-demo2"
+      SystemName = "tfc-demo"
     }
   }
 }
@@ -20,7 +20,7 @@ terraform {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
 
-  name = "stg-vpc"
+  name = "stg-vpc-test-01"
   cidr = "10.10.0.0/16"
 
   azs                       = ["ap-northeast-1a", "ap-northeast-1c"]
